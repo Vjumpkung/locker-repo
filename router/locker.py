9f4514e1654a19c2f06c8f889a43889cb33b1d3f
Siripa-Maneein <siripa.m@ku.th>
2023-02-07 14:32:19 +0700

    reserve_locker

--- router/locker.py
+++ router/locker.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from config import database
 import datetime
+from fastapi import FastAPI, HTTPException, Body
+from router.body_template import Reservation
 
 router = APIRouter(prefix="/locker", tags=["locker"])
 cur = database.client["exceed06"]["Locker"]
@@ -21,3 +23,25 @@ def get_all_locker():
             dic["time_left"] = "late : " + str(current_time - end_time).split(".")[0]
         lst.append(dic)
     return lst
+
+
+@router.post("/reserve")
+def reserve_locker(reservation: Reservation):
+    locker_id = reservation.locker_id
+    locker = cur.find_one({"locker_id": reservation.locker_id})
+    if locker["is_available"]:
+        expected_duration = datetime.timedelta(hours=reservation.hour, minutes=reservation.minute)
+        if expected_duration > datetime.timedelta(hours=2):
+            time_diff = expected_duration - datetime.timedelta(hours=2)
+            cost = time_diff.total_seconds()//60//60 * 5
+        else:
+            cost = 0
+        cur.update_many({"locker_id": locker_id}, {'$set': {"std_id": reservation.std_id,
+                                                            "time_start": datetime.datetime.now(),
+                                                            "time_end": datetime.datetime.now() + expected_duration,
+                                                            "is_available": False,
+                                                            "contain": reservation.contain,
+                                                            "cost": cost
+                                                            }})
+    else:
+        raise HTTPException(status_code=400, detail="Sorry, Locker is not available.")
