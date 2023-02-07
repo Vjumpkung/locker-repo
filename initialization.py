57588cb462c6de743ffe0f775db3184178057706
vjumpkung <vjumpkung@gmail.com>
2023-02-07 14:51:15 +0700

    edit init file

--- initialization.py
+++ initialization.py
@@ -12,7 +12,7 @@ lst = []
 default_start_time = datetime.datetime.now()
 default_stop_time = datetime.datetime.now() + datetime.timedelta(hours=2)
 
-for i in range(3):
+for i in range(6):
     dic = {
         "locker_id": i + 1,
         "std_id": None,
@@ -23,38 +23,4 @@ for i in range(3):
         "is_available": True,
     }
     lst.append(dic)
-
-dic = {
-    "locker_id": 4,
-    "std_id": 123,
-    "contain": ["battery", "monitor"],
-    "time_start": default_stop_time,
-    "time_end": default_start_time - datetime.timedelta(minutes=10),
-    "cost": 0,
-    "is_available": False,
-}
-lst.append(dic)
-
-dic = {
-    "locker_id": 5,
-    "std_id": 12,
-    "contain": [],
-    "time_start": default_stop_time,
-    "time_end": default_start_time - datetime.timedelta(days=11, minutes=10),
-    "cost": 0,
-    "is_available": False,
-}
-lst.append(dic)
-
-dic = {
-    "locker_id": 6,
-    "std_id": 5,
-    "contain": ["bag", "phone"],
-    "time_start": default_start_time,
-    "time_end": default_stop_time,
-    "cost": 0,
-    "is_available": False,
-}
-lst.append(dic)
-
 cur.insert_many(lst)
